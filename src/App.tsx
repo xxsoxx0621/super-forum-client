6442cb52a4ced905677993e9fbc2b552f79ec5a7
sohyeon <xxsoxx0621@github.com>
2023-03-07 17:41:23 +0900

    feature : make structure and adjust mediaQuery

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Nav from "./components/Nav";
+import Main from "./components/Main";
+import LeftMenu from "./components/LeftMenu";
+import RightMenu from "./components/RightMenu";
+import SideBar from "./components/SideBar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <SideBar />
+      <LeftMenu />
+      <Main />
+      <RightMenu />
     </div>
   );
 }
