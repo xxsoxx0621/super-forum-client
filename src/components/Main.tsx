b04a29397b529e0e7e64e2620343d74482829bb1
sohyeon <xxsoxx0621@github.com>
2023-03-07 18:00:03 +0900

    feature : add ErrorBoundary

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Main = () => {
-    return <main className="content">Main</main>
+    const test = true;
+
+    if(test) throw new Error("Main fail");
+    else {
+        return <main className="content">Main</main>
+    }
 };
 
 export default Main;
